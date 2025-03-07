858d397c3cfedb481708dea9788b88908fe874ec
jwheller0013 <jwheller0013@yahoo.com>
2025-03-06 19:16:05 -0500

    Unsure how to test foo

--- src/test/WriteLoopsTest.java
+++ src/test/WriteLoopsTest.java
@@ -131,4 +131,15 @@ public class WriteLoopsTest
         WriteLoops writeLoo1 = new WriteLoops();
         assertEquals(8, writeLoo1.loop50by7());
     }
+
+//@Test
+//public void foo()
+//{
+//    WriteLoops writeLoo1 = new WriteLoops();
+//    assertEquals("The Sum is 84" n\ 7, writeLoo1.foo());
+//}
+//    Unsure how to test the multiple lines of text it would print
+
+
+
 }
